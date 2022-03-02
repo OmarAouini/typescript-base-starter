d368f22bdd30bfddb729566797609f3486e8f3fe
OmarAouini <30733260+OmarAouini@users.noreply.github.com>
2022-03-02 15:09:33 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -9,8 +9,10 @@ export enum Roles {
 
 const HOST = process.env.HOST || "0.0.0.0"
 const PORT = process.env.PORT || 8080
+const KEYCLOAK_BASE_URL = process.env.KEYCLOAK_BASE_URL || ""
+const KEYLOAK_REALM = "compress"
 const KEYCLOAK_PUBLIC_KEY = () => {
-  let res = request('GET', 'http://localhost:8080/auth/realms/appsdeveloperblog');
+  let res = request('GET', `${KEYCLOAK_BASE_URL}/auth/realms/${KEYLOAK_REALM}`);
   let response = JSON.parse(res.getBody().toString());
   let publicKey = `-----BEGIN PUBLIC KEY-----\r\n${response.public_key}\r\n-----END PUBLIC KEY-----`;
   return publicKey;
