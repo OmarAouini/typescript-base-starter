0c397c0e0c38b4f5be5cf1f0fd12800e5f252fe0
OmarAouini <30733260+OmarAouini@users.noreply.github.com>
2022-03-02 14:36:09 +0100

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -7,4 +7,6 @@ export enum Roles {
   CAMERAMAN="cameraman"
 }
 
+const HOST = process.env.HOST || "0.0.0.0"
+const PORT = process.env.PORT || 8080
 const KEYCLOAK_PUBLIC_KEY = process.env.KEYCLOAK_PUBLIC_KEY || ""
